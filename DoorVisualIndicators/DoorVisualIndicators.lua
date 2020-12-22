7467b5451c1de1844b220a7d7cb0c3a9047adb7e
ellomenop <ellomenop.speedruns@gmail.com>
2020-12-21 23:21:03 -0500

    Fixing Barge and Adding Miniboss Ghost Previews

--- DoorVisualIndicators/DoorVisualIndicators.lua
+++ DoorVisualIndicators/DoorVisualIndicators.lua
@@ -13,21 +13,39 @@ DoorVisualIndicators.MiniBossLabels = {
   A_MiniBoss04 = "Doomstone",
   B_Wrapping01 = "Barge",
   B_MiniBoss01 = "Power Couple",
-  B_MiniBoss02 = "Witches",
+  B_MiniBoss02 = "4 Witches",
   C_MiniBoss01 = "Asterius",
   C_MiniBoss02 = "Soul Catcher",
-  D_MiniBoss01 = "Satyr",
+  D_MiniBoss01 = "Satyr Cultist",
   D_MiniBoss02 = "Rat Thug",
-  -- TODO: Find out to individually identify (Probably not possible without breaking modularity with RoomDeterminism)
+  -- TODO: Find out how to individually identify (Probably not possible without breaking modularity with RoomDeterminism)
   D_MiniBoss03 = "Tiny Vermin / Snakestone",
   D_MiniBoss04 = "Bother",
 }
 
+DoorVisualIndicators.MiniBossAnimations = {
+  A_MiniBoss01 = {"Enemy_BloodlessGrenadierIdle"},
+  A_MiniBoss02 = {"HeavyRangedSplitterCrystal"},
+  A_MiniBoss03 = {"EnemyWretchAssassin_Idle"},
+  A_MiniBoss04 = {"HeavyRangedSplitterCrystal"},
+  -- "AsphodelWrappingTile01", "AsphodelBoat01", "AsphodelBoatSunkUnlocked",
+  B_Wrapping01 = {"HealRangedCrystal4"},
+  B_MiniBoss01 = {"CrusherUnitIdle", "EnemyMedusaHeadIdle"},
+  B_MiniBoss02 = {"EnemyWretchCasterIdle_SpreadShot"}, -- x4 and make closer together?
+  C_MiniBoss01 = {"Minotaur_Idle"},
+  C_MiniBoss02 = {"SoulSpawnerIdle"},
+  D_MiniBoss01 = {"SatyrMinibossIdle"},
+  D_MiniBoss02 = {"EnemyRatThugIdle"},
+  -- TODO: Find out how to individually identify (Probably not possible without breaking modularity with RoomDeterminism)
+  D_MiniBoss03 = {"EnemyCrawlerIdle", "HeavyRangedForkedCrystal4"},
+  D_MiniBoss04 = {"EnemyStyxThiefIdle"},
+}
 
 ModUtil.WrapBaseFunction("CreateDoorPreviewIcon", function ( baseFunc, exitDoor, args )
   baseFunc(exitDoor, args)
 
   local room_name = ModUtil.PathGet('RoomData.Name', args) or ""
+  local is_miniboss = RoomData[room_name].IsMiniBossRoom or false
 
   -- If the room is a fountain, add a visual effect to the door icon
   if string.match(room_name, "[ABC]_Reprieve") and config.ShowFountainDoorIndictor then
@@ -37,10 +55,39 @@ ModUtil.WrapBaseFunction("CreateDoorPreviewIcon", function ( baseFunc, exitDoor,
       Color = Color.Turquoise,
       Loop = true})
 
-  -- If the room is a miniboss, add a text label of the miniboss name above the door
-  elseif string.find(room_name, "MiniBoss") and DoorVisualIndicators.MiniBossLabels[room_name] ~= nil and config.ShowMinibossDoorIndicator then
+  -- If the room is a miniboss, add a text label of the miniboss name above the door and a ghostly preview
+elseif is_miniboss and DoorVisualIndicators.MiniBossLabels[room_name] ~= nil and config.ShowMinibossDoorIndicator then
+    -- Attach an obstacle to the door that subsequent UI elements can be attached to
+    local test = SpawnObstacle({ Name = "BlankGeoObstacle", Group = "Test" })
+    table.insert( exitDoor.AdditionalAttractIds, test )
+    Attach({ Id = test, DestinationId = exitDoor.DoorIconId, DynamicScaleOffset = true })
+
+    -- Dynamically place the ghosts based on how many will be shown (e.g. power couple have two ghosts)
+    local number_of_ghosts = #DoorVisualIndicators.MiniBossAnimations[room_name]
+    local angle = -120
+
+    local base_offset = (number_of_ghosts - 1) * -50
+    local isometric_adjust_angle = 63
+    if IsHorizontallyFlipped({ Id = exitDoor.ObjectId }) then
+      angle = -30
+      isometric_adjust_angle = -60
+    end
+    -- For each ghost, draw it above the door
+    for num, ghost in ipairs(DoorVisualIndicators.MiniBossAnimations[room_name]) do
+      CreateAnimation({ -- TODO: Opacity?
+        Name = ghost,
+        DestinationId = test,
+        Color = Color.Gray,
+        OffsetY = (base_offset + (num - 1) * 100) * math.cos(math.rad(isometric_adjust_angle)),
+        OffsetX = (base_offset + (num - 1) * 100) * math.sin(math.rad(isometric_adjust_angle)),
+        OffsetZ = 100,
+        Loop = true})
+      SetAngle({ Id = test, Angle = angle })
+    end
+
+    -- Add one text label of the miniboss name
     CreateTextBox({
-      Id = exitDoor.DoorIconFront,
+      Id = test,
       Text = DoorVisualIndicators.MiniBossLabels[room_name],
       OffsetY = -20,
       FontSize = 14,
