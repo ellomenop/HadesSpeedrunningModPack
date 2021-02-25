c2924da06485d0899fa615a3ff7045ad25f39b7b
Museus <69139347+Museus@users.noreply.github.com>
2021-02-25 17:18:19 -0500

    Remove Cutscenes (#16)
    * outro: remove cutscene
    
    * Add Remove Cutscenes Mod
    
    All authoring credit to Museus

---
+++ RemoveOutroMod/RemoveOutroMod.lua
@@ -0,0 +1,58 @@
+--[[
+    RemoveCutscenes
+    Author:
+        Museus (Discord: Museus#7777)
+
+    Optionally removes intro and outro cutscenes to runs
+]]
+ModUtil.RegisterMod("RemoveCutscenes")
+
+local config = {
+    ModName = "RemoveCutscenes",
+    RemoveIntro = true,
+    RemoveOutro = true,
+}
+RemoveCutscenes.config = config
+
+if ModConfigMenu then
+  ModConfigMenu.Register(config)
+end
+
+-- Remove starting cutscene
+ModUtil.WrapBaseFunction("ShowRunIntro", function( baseFunc )
+    if config.RemoveIntro then
+        return
+    end
+
+    baseFunc()
+end, RemoveCutscenes)
+
+
+ModUtil.WrapBaseFunction("EndEarlyAccessPresentation", function ( baseFunc )
+    if config.RemoveOutro then
+        CurrentRun.ActiveBiomeTimer = false
+
+        thread( Kill, CurrentRun.Hero )
+        wait( 0.15 )
+
+        FadeIn({ Duration = 0.5 })
+        return
+    end
+
+    baseFunc()
+end, RemoveCutscenes)
+
+
+-- Scripts/RoomManager.lua : 1874
+ModUtil.WrapBaseFunction("StartRoom", function ( baseFunc, currentRun, currentRoom )
+    PrintUtil.showModdedWarning()
+
+    baseFunc(currentRun, currentRoom)
+end, RemoveCutscenes)
+
+-- Scripts/UIScripts.lua : 145
+ModUtil.WrapBaseFunction("ShowCombatUI", function ( baseFunc, flag )
+    PrintUtil.showModdedWarning()
+
+    baseFunc(flag)
+end, RemoveCutscenes)
