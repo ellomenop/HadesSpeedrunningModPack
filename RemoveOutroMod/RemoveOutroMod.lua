e2dab07d48db27d132bd89002a85b67cc25f8108
ellomenop <ellomenop.speedruns@gmail.com>
2021-02-25 15:23:32 -0500

    Add Remove Cutscenes Mod
    All authoring credit to Museus

--- RemoveOutroMod/RemoveOutroMod.lua
+++ RemoveOutroMod/RemoveOutroMod.lua
@@ -1,10 +1,58 @@
-ModUtil.RegisterMod("RemoveOutroMod")
+--[[
+    RemoveCutscenes
+    Author:
+        Museus (Discord: Museus#7777)
 
-ModUtil.BaseOverride("EndEarlyAccessPresentation", function ()
-	CurrentRun.ActiveBiomeTimer = false
-	
-	thread( Kill, CurrentRun.Hero )
-	wait( 0.15 )
+    Optionally removes intro and outro cutscenes to runs
+]]
+ModUtil.RegisterMod("RemoveCutscenes")
 
-	FadeIn({ Duration = 0.5 })
-end, RemoveOutroMod)
+local config = {
+    ModName = "RemoveCutscenes",
+    RemoveIntro = true,
+    RemoveOutro = true,
+}
+RemoveCutscenes.config = config
+
+if ModConfigMenu then
+  ModConfigMenu.Register(config)
+end
+
+-- Remove starting cutscene
+ModUtil.WrapBaseFunction("ShowRunIntro", function( baseFunc )
+    if config.RemoveIntro then
+        return
+    end
+
+    baseFunc()
+end, RemoveCutscenes)
+
+
+ModUtil.WrapBaseFunction("EndEarlyAccessPresentation", function ( baseFunc )
+    if config.RemoveOutro then
+        CurrentRun.ActiveBiomeTimer = false
+
+        thread( Kill, CurrentRun.Hero )
+        wait( 0.15 )
+
+        FadeIn({ Duration = 0.5 })
+        return
+    end
+
+    baseFunc()
+end, RemoveCutscenes)
+
+
+-- Scripts/RoomManager.lua : 1874
+ModUtil.WrapBaseFunction("StartRoom", function ( baseFunc, currentRun, currentRoom )
+    PrintUtil.showModdedWarning()
+
+    baseFunc(currentRun, currentRoom)
+end, RemoveCutscenes)
+
+-- Scripts/UIScripts.lua : 145
+ModUtil.WrapBaseFunction("ShowCombatUI", function ( baseFunc, flag )
+    PrintUtil.showModdedWarning()
+
+    baseFunc(flag)
+end, RemoveCutscenes)
