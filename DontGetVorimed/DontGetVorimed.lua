91955839d69946a840b34b4eb25a8c1a38dd5e54
ellomenop <ellomenop.speedruns@gmail.com>
2021-02-25 22:16:53 -0500

    Implemented anti-vorime'd technology
    Makes first roll of first boon reward offer all 4 core boons

---
+++ DontGetVorimed/DontGetVorimed.lua
@@ -0,0 +1,32 @@
+--[[
+    DontGetVorimed
+    Authors:
+    Ellomenop (Discord: ellomenop#2254)
+    Makes the first room reward offer all 4 core boons
+]]
+ModUtil.RegisterMod("DontGetVorimed")
+
+local config = {
+    ModName = "Dont Get Vorimed",
+    Enabled = true
+}
+DontGetVorimed.config = config
+
+-- When the first room is created, set the number of loot choices to 4
+ModUtil.WrapBaseFunction("ChooseStartingRoom", function ( baseFunc, currentRun, roomSetName )
+  LootChoiceExt.Choices = 4
+  return baseFunc(currentRun, roomSetName)
+end, DontGetVorimed)
+
+-- After first reward has been selected, return to normal number of choices
+ModUtil.WrapBaseFunction("HandleUpgradeChoiceSelection", function ( baseFunc, screen, button )
+  LootChoiceExt.Choices = 3
+  baseFunc(screen, button)
+end, DontGetVorimed)
+
+-- If the player rerolls, reduce to 3 options
+ModUtil.WrapBaseFunction("DestroyBoonLootButtons", function ( baseFunc, lootData )
+  baseFunc(lootData)
+  LootChoiceExt.Choices = 3
+  LootChoiceExt.LastLootChoices = 3
+end, DontGetVorimed)
