cd2062a4eb3c4e6647b7d58f366533ce833c8b3a
ellomenop <ellomenop.speedruns@gmail.com>
2021-02-25 22:28:06 -0500

    Adding ModConfigMenu support

--- DontGetVorimed/DontGetVorimed.lua
+++ DontGetVorimed/DontGetVorimed.lua
@@ -12,21 +12,31 @@ local config = {
 }
 DontGetVorimed.config = config
 
+if ModConfigMenu then
+  ModConfigMenu.Register(config)
+end
+
 -- When the first room is created, set the number of loot choices to 4
 ModUtil.WrapBaseFunction("ChooseStartingRoom", function ( baseFunc, currentRun, roomSetName )
-  LootChoiceExt.Choices = 4
+  if config.Enabled then
+    LootChoiceExt.Choices = 4
+  end
   return baseFunc(currentRun, roomSetName)
 end, DontGetVorimed)
 
 -- After first boon reward has been selected, return to normal number of choices
 ModUtil.WrapBaseFunction("HandleUpgradeChoiceSelection", function ( baseFunc, screen, button )
-  LootChoiceExt.Choices = 3
+  if config.Enabled then
+    LootChoiceExt.Choices = 3
+  end
   baseFunc(screen, button)
 end, DontGetVorimed)
 
 -- If the player ever rerolls, reduce to 3 options
 ModUtil.WrapBaseFunction("DestroyBoonLootButtons", function ( baseFunc, lootData )
   baseFunc(lootData)
-  LootChoiceExt.Choices = 3
-  LootChoiceExt.LastLootChoices = 3
+  if config.Enabled then
+    LootChoiceExt.Choices = 3
+    LootChoiceExt.LastLootChoices = 3
+  end
 end, DontGetVorimed)
