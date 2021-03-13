125e7f9275ca6cc55ffb0d19c800a0c6450e99da
ellomenop <ellomenop.speedruns@gmail.com>
2021-03-12 23:01:15 -0500

    Added Thanatos Mod
    Combined functionality of remove thanatos and rebalance thanatos mods

---
+++ ThanatosMod/ThanatosMod.lua
@@ -0,0 +1,97 @@
+--[[
+    ThanatosMod v1.0
+    Author:
+        Museus (Discord: Museus#7777)
+        ellomenop (Discord: ellomenop#2254)
+
+    Gives options to modify or remove Thanatos
+]]
+
+ModUtil.RegisterMod("ThanatosMod")
+
+local config = {
+    ThanatosSetting = "Rebalanced"
+}
+ThanatosMod.config = config
+
+ThanatosMod.Presets = {
+    Removed = {},
+    Rebalanced = {
+        Tartarus = {
+            BaseDifficulty = 94, -- Vanilla 200
+            EliteScaling = 12, -- Vanilla 16
+            MinWaves = 2, -- Vanilla 3
+            MaxWaves = 2, -- Vanilla 3
+        },
+        Asphodel = {
+            BaseDifficulty = 325, -- Vanilla 400
+            EliteScaling = 30, -- Vanilla 40
+            MinWaves = 2, -- Vanilla 3
+            MaxWaves = 2, -- Vanilla 3
+        },
+        Elysium = {
+            BaseDifficulty = 514, -- Vanilla 575
+            EliteScaling = 80, -- Vanilla 110
+            MinWaves = 2, -- Vanilla 3
+            MaxWaves = 2, -- Vanilla 3
+        }
+    },
+    Vanilla = {
+        Tartarus = {
+            BaseDifficulty = 200,
+            EliteScaling = 16,
+            MinWaves = 3,
+            MaxWaves = 3,
+        },
+        Asphodel = {
+            BaseDifficulty = 400,
+            EliteScaling = 40,
+            MinWaves = 3,
+            MaxWaves = 3,
+        },
+        Elysium = {
+            BaseDifficulty = 575,
+            EliteScaling = 110,
+            MinWaves = 3,
+            MaxWaves = 3,
+        }
+    },
+}
+
+OnAnyLoad{ function()
+    if config.ThanatosSetting == "Removed" then
+        ModUtil.MapSetTable(EncounterData, {
+            BaseThanatos = {
+                MaxThanatosSpawnsThisRun = 0,
+            },
+        })
+    else
+      ModUtil.MapSetTable(EncounterData, {
+          BaseThanatos = {
+              MaxThanatosSpawnsThisRun = 1,
+          },
+      })
+    end
+end}
+
+function updateThanatosValues(data)
+    local biome = string.sub(data.Name, 9, #data.Name)
+    data.BaseDifficulty = ThanatosMod.Presets[config.ThanatosSetting][biome].BaseDifficulty
+    data.HardEncounterOverrideValues = {
+        DepthDifficultyRamp = ThanatosMod.Presets[config.ThanatosSetting][biome].EliteScaling
+    }
+    data.MinWaves = ThanatosMod.Presets[config.ThanatosSetting][biome].MinWaves
+    data.MaxWaves = ThanatosMod.Presets[config.ThanatosSetting][biome].MaxWaves
+end
+
+ModUtil.WrapBaseFunction("SetupEncounter", function( baseFunc, encounterData, room )
+    if config.ThanatosSetting ~= "Vanilla" and config.ThanatosSetting ~= "Removed" then
+      local moddedEncounter = DeepCopyTable( encounterData )
+      if string.match(moddedEncounter.Name, "Thanatos") then
+  			updateThanatosValues(moddedEncounter)
+  		end
+      return baseFunc( moddedEncounter, room )
+    end
+
+    return baseFunc( encounterData, room )
+end, ThanatosMod)
