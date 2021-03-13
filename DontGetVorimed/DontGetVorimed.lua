a094473aa9ad512c14067f24cf5b988e494ef632
ellomenop <69175026+ellomenop@users.noreply.github.com>
2021-03-12 22:55:26 -0500

    Implemented anti-vorime'd technology (#23)
    * Implemented anti-vorime'd technology
    
    Makes first roll of first boon reward offer all 4 core boons
    
    * Added LootChoiceExt dependency
    
    Credit to Magic Gonads
    
    * Minor documentation fixes

---
+++ DontGetVorimed/DontGetVorimed.lua
@@ -0,0 +1,39 @@
+--[[
+    DontGetVorimed
+    Authors:
+    Ellomenop (Discord: ellomenop#2254)
+    Makes the first boon reward offer all 4 core boons
+]]
+ModUtil.RegisterMod("DontGetVorimed")
+
+local config = {
+    ModName = "Dont Get Vorimed",
+    Enabled = true
+}
+DontGetVorimed.config = config
+
+-- When the first room is created, set the number of loot choices to 4
+-- This will persist until the first boon reward is received or rolled over
+ModUtil.WrapBaseFunction("ChooseStartingRoom", function ( baseFunc, currentRun, roomSetName )
+  if config.Enabled then
+    LootChoiceExt.Choices = 4
+  end
+  return baseFunc(currentRun, roomSetName)
+end, DontGetVorimed)
+
+-- After first boon reward has been selected, return to normal number of choices
+ModUtil.WrapBaseFunction("HandleUpgradeChoiceSelection", function ( baseFunc, screen, button )
+  if config.Enabled then
+    LootChoiceExt.Choices = 3
+  end
+  baseFunc(screen, button)
+end, DontGetVorimed)
+
+-- If the player ever rerolls, reduce to 3 options
+ModUtil.WrapBaseFunction("DestroyBoonLootButtons", function ( baseFunc, lootData )
+  baseFunc(lootData)
+  if config.Enabled then
+    LootChoiceExt.Choices = 3
+    LootChoiceExt.LastLootChoices = 3
+  end
+end, DontGetVorimed)
