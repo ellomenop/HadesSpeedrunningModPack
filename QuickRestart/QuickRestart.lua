b0de5b15e68fa1610c5cca54c578f660ede8a1a8
ellomenop <ellomenop.speedruns@gmail.com>
2021-03-15 00:57:44 -0400

    Changed Bind and Fixed Key Ordering Issue

--- QuickRestart/QuickRestart.lua
+++ QuickRestart/QuickRestart.lua
@@ -5,10 +5,48 @@ local config = {
 }
 QuickRestart.config = config
 
-OnControlPressed{ "Codex",
+OnControlPressed{ "Assist",
   function(triggerArgs)
     if config.Enabled and not ModUtil.PathGet("CurrentDeathAreaRoom") then
-      if IsControlDown({ Name = "Codex" })
+      if IsControlDown({ Name = "Assist" })
+          and IsControlDown({ Name = "Use" })
+          and IsControlDown({ Name = "Shout" })
+          and IsControlDown({ Name = "Reload" }) then
+        KillHero(CurrentRun.Hero, triggerArgs)
+      end
+    end
+end}
+
+OnControlPressed{ "Use",
+  function(triggerArgs)
+    if config.Enabled and not ModUtil.PathGet("CurrentDeathAreaRoom") then
+      if IsControlDown({ Name = "Assist" })
+          and IsControlDown({ Name = "Use" })
+          and IsControlDown({ Name = "Shout" })
+          and IsControlDown({ Name = "Reload" }) then
+        KillHero(CurrentRun.Hero, triggerArgs)
+      end
+    end
+end}
+
+OnControlPressed{ "Shout",
+  function(triggerArgs)
+    if config.Enabled and not ModUtil.PathGet("CurrentDeathAreaRoom") then
+      if IsControlDown({ Name = "Assist" })
+          and IsControlDown({ Name = "Use" })
+          and IsControlDown({ Name = "Shout" })
+          and IsControlDown({ Name = "Reload" }) then
+        KillHero(CurrentRun.Hero, triggerArgs)
+      end
+    end
+end}
+
+OnControlPressed{ "Reload",
+  function(triggerArgs)
+    if config.Enabled and not ModUtil.PathGet("CurrentDeathAreaRoom") then
+      if IsControlDown({ Name = "Assist" })
+          and IsControlDown({ Name = "Use" })
+          and IsControlDown({ Name = "Shout" })
           and IsControlDown({ Name = "Reload" }) then
         KillHero(CurrentRun.Hero, triggerArgs)
       end
