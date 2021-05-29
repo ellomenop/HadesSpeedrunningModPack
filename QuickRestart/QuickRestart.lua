32a1307984861a397ed95bf342d99e5abe0d1ecf
Museus <museus@protonmail.com>
2021-05-29 14:32:58 -0400

    QuickRestart: set conditions for illegal restarts to avoid bugs, print overhead

--- QuickRestart/QuickRestart.lua
+++ QuickRestart/QuickRestart.lua
@@ -5,50 +5,75 @@ local config = {
 }
 QuickRestart.config = config
 
+function QuickRestart.CanReset()
+  -- QuickRestart must be Enabled
+  if not config.Enabled then return false end
+
+  -- Zag must not be in the House
+  if ModUtil.PathGet("CurrentDeathAreaRoom") then return false end
+
+  -- Zag must not be frozen
+  if not IsEmpty( CurrentRun.Hero.FreezeInputKeys ) then return false end
+
+  -- Combat UI must be visible
+	if not (ShowingCombatUI or false) then return false end
+
+  return true
+end
+
+function QuickRestart.ResetRun(triggerArgs)
+  if not QuickRestart.CanReset() then
+    ModUtil.Hades.PrintOverhead("Can't reset now!", 2)
+    return
+  end
+
+  KillHero( CurrentRun.Hero, triggerArgs )
+end
+
 OnControlPressed{ "Assist",
   function(triggerArgs)
-    if config.Enabled and not ModUtil.PathGet("CurrentDeathAreaRoom") then
+    if config.Enabled then
       if IsControlDown({ Name = "Assist" })
           and IsControlDown({ Name = "Use" })
           and IsControlDown({ Name = "Shout" })
           and IsControlDown({ Name = "Reload" }) then
-        KillHero(CurrentRun.Hero, triggerArgs)
+        QuickRestart.ResetRun(triggerArgs)
       end
     end
 end}
 
 OnControlPressed{ "Use",
   function(triggerArgs)
-    if config.Enabled and not ModUtil.PathGet("CurrentDeathAreaRoom") then
+    if config.Enabled then
       if IsControlDown({ Name = "Assist" })
           and IsControlDown({ Name = "Use" })
           and IsControlDown({ Name = "Shout" })
           and IsControlDown({ Name = "Reload" }) then
-        KillHero(CurrentRun.Hero, triggerArgs)
+        QuickRestart.ResetRun(triggerArgs)
       end
     end
 end}
 
 OnControlPressed{ "Shout",
   function(triggerArgs)
-    if config.Enabled and not ModUtil.PathGet("CurrentDeathAreaRoom") then
+    if config.Enabled then
       if IsControlDown({ Name = "Assist" })
           and IsControlDown({ Name = "Use" })
           and IsControlDown({ Name = "Shout" })
           and IsControlDown({ Name = "Reload" }) then
-        KillHero(CurrentRun.Hero, triggerArgs)
+        QuickRestart.ResetRun(triggerArgs)
       end
     end
 end}
 
 OnControlPressed{ "Reload",
   function(triggerArgs)
-    if config.Enabled and not ModUtil.PathGet("CurrentDeathAreaRoom") then
+    if config.Enabled then
       if IsControlDown({ Name = "Assist" })
           and IsControlDown({ Name = "Use" })
           and IsControlDown({ Name = "Shout" })
           and IsControlDown({ Name = "Reload" }) then
-        KillHero(CurrentRun.Hero, triggerArgs)
+        QuickRestart.ResetRun(triggerArgs)
       end
     end
 end}
