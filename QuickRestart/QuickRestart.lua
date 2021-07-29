36174b42462c2560e7616f0e1029d3a4c29ee2f3
Museus <69139347+Museus@users.noreply.github.com>
2021-07-29 12:01:56 -0400

    QuickRestart: Disallow in Thanatos Encounters until enemies spawn (#52)
    * QuickRestart: Disallow in Thanatos Encounters until enemies spawn
    
    * !fixup add nil-checking to Thanatos in CanReset

--- QuickRestart/QuickRestart.lua
+++ QuickRestart/QuickRestart.lua
@@ -16,7 +16,15 @@ function QuickRestart.CanReset()
   if not IsEmpty( CurrentRun.Hero.FreezeInputKeys ) then return false end
 
   -- Combat UI must be visible
-	if not (ShowingCombatUI or false) then return false end
+  if not (ShowingCombatUI or false) then return false end
+
+  -- If we're in a Thanatos Room, enemies must have already spawned
+  if (CurrentRun ~= nil and CurrentRun.CurrentRoom ~= nil and
+      CurrentRun.CurrentRoom.Encounter ~= nil and
+      CurrentRun.CurrentRoom.Encounter.ThanatosId ~= nil
+      and GetActiveEnemyCount() == 0) then
+    return false
+  end
 
   return true
 end
