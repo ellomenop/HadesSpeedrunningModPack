a5647396d1cbdf5197b4e85d61e6fd2b3fac475d
Kenorah <j.j.kennedy.amor@gmail.com>
2022-10-20 02:53:34 +1100

    Added CodexTechPrevention
    Prevents the user from opening the codex while the in-game timer is running, thus preventing the use of "CodexTech"

---
+++ CodexTechPrevention/CodexTechPrevention.lua
@@ -0,0 +1,25 @@
+ModUtil.RegisterMod("CodexTechPrevention")
+
+local config = {
+    ModName = "Codex Tech Prevention",
+    Enabled = true,
+}
+
+CodexTechPrevention.config = config
+
+-- Scripts/CodexScripts.lua : 1056
+-- Prevents the use of CodexTech by preventing the codex opening while the timer is running in a run
+ModUtil.WrapBaseFunction("CanOpenCodex", function ( baseFunc )
+    if CodexTechPrevention.config.Enabled then
+        if not CurrentRun.Hero.IsDead then
+            timerRunningAndOutOfCombat = not HasTimerBlock( CurrentRun ) and not IsCombatEncounterActive ( CurrentRun )
+
+            if timerRunningAndOutOfCombat then
+                ModUtil.Hades.PrintOverhead("Can't open the codex now!", 2)
+                return false
+            end
+        end
+    end
+
+    return baseFunc()
+end, CodexTechPrevention)
