7a77e2bec8e0537022fa7c02bdcb7406a8e09206
Zyruvias <zerianledrias@gmail.com>
2022-11-30 08:59:15 -0500

    use wraps

--- QuickRestart/QuickRestart.lua
+++ QuickRestart/QuickRestart.lua
@@ -129,87 +129,29 @@ OnAnyLoad{ "RoomPreRun",
     end
 }
 
-ModUtil.Path.Override( "HandleDeath", function( currentRun, killer, killingUnitWeapon )
-    if GetConfigOptionValue({ Name = "EditingMode" }) then
-        SetAnimation({ Name = "ZagreusDeadStartBlood", DestinationId = currentRun.Hero.ObjectId })
-        return
-    end
-
+ModUtil.Path.Wrap("HandleDeath", function(baseFunc, currentRun, killer, killingUnitWeapon)
     if QuickRestart.UsedQuickRestart then
         RemoveInputBlock({ Name = "QuickRestart" })
     end
+    QuickRestart.QuickDeathApplicable = config.QuickDeathEnabled and not currentRun.Cleared
+    return baseFunc(currentRun, killer, killingUnitWeapon)
+end, QuickRestart)
 
-    SendSaveFileEmail({ })
+ModUtil.Path.Context.Wrap("HandleDeath", function ()
+    ModUtil.Path.Wrap("LoadMap", function(baseFunc, argTable)
+        if QuickRestart.UsedQuickRestart or QuickRestart.QuickDeathApplicable then
+            argTable.Name = "RoomPreRun"
 
-    AddTimerBlock( currentRun, "HandleDeath" )
-    if ScreenAnchors.TraitTrayScreen ~= nil then
-        CloseAdvancedTooltipScreen()
-    end
-    ClearHealthShroud()
-    CurrentRun.Hero.HandlingDeath = true
-    CurrentRun.Hero.IsDead = true
-    CurrentRun.ActiveBiomeTimer = false
-    if ConfigOptionCache.EasyMode and not currentRun.Cleared then
-        GameState.EasyModeLevel = GameState.EasyModeLevel + 1
-    end
-    if not CurrentRun.Cleared then -- Already recorded if cleared
-        RecordRunStats()
-    end
+            if QuickRestart.KeepStartingKeepsake and GameState.QuickRestartStartingKeepsake then
+              GameState.LastAwardTrait = GameState.QuickRestartStartingKeepsake
+            end
+          end
 
-    InvalidateCheckpoint()
-
-    ZeroSuperMeter()
-    FinishTargetMarker( killer )
-
-    local deathPresentationName = currentRun.DeathPresentationFunctionName or "DeathPresentation"
-    local deathPresentationFunction = _G[deathPresentationName]
-    deathPresentationFunction( currentRun, killer, killingUnitWeapon )
-    AddInputBlock({ Name = "MapLoad" })
-
-    currentRun.CurrentRoom.EndingHealth = currentRun.Hero.Health
-    currentRun.EndingMoney = currentRun.Money
-    table.insert( currentRun.RoomHistory, currentRun.CurrentRoom )
-    UpdateRunHistoryCache( currentRun, currentRun.CurrentRoom )
-
-    currentRun.Money = 0
-    currentRun.NumRerolls = GetNumMetaUpgrades( "RerollMetaUpgrade" ) + GetNumMetaUpgrades("RerollPanelMetaUpgrade")
-
-    ResetObjectives()
-    ActiveScreens = {}
-
-    CurrentRun.Hero.HandlingDeath = false
-    CurrentRun.Hero.Health = CurrentRun.Hero.MaxHealth
-
-    local currentRoom = currentRun.CurrentRoom
-    local deathMap = "DeathArea"
-
-    -- Changes Here
-    local quickDeathApplicable = config.QuickDeathEnabled and not currentRun.Cleared
-    if QuickRestart.UsedQuickRestart or quickDeathApplicable then
-      deathMap = "RoomPreRun"
-      
-      if QuickRestart.KeepStartingKeepsake and GameState.QuickRestartStartingKeepsake then
-        GameState.LastAwardTrait = GameState.QuickRestartStartingKeepsake
-      end
-    end
-
-    -- Set UsedQuickRestart Flag so Keepsake and InputBlock are appropriately set.
-    if quickDeathApplicable then
-        QuickRestart.UsedQuickRestart = true
-    end
-    -- End Changes
-
-    GameState.LocationName = "Location_Home"
-    RandomSetNextInitSeed()
-    SaveCheckpoint({ StartNextMap = deathMap, DevSaveName = CreateDevSaveName( currentRun, { StartNextMap = deathMap } ) })
-    ClearUpgrades()
-
-    SetConfigOption({ Name = "FlipMapThings", Value = false })
-
-    local runNumber = ( GetCompletedRuns() + 1 )
-    local runDepth = GetRunDepth( currentRun )
-
-    LoadMap({ Name = deathMap, ResetBinks = true, ResetWeaponBinks = true })
+          -- Set UsedQuickRestart Flag so Keepsake and InputBlock are appropriately set.
+          if quickDeathApplicable then
+              QuickRestart.UsedQuickRestart = true
+          end
+    end, QuickRestart)
 end, QuickRestart)
 
 ModUtil.Path.Wrap("WindowDropEntrance", function( baseFunc, ... )
