8aab39f82616c1f98c0ca2b225356248bdba131d
Museus <museus@protonmail.com>
2022-12-23 12:26:44 -0500

    refactor(UI): update sack UI to use ModpackMenu.DropDown

--- HadesSpeedrunningModpack/RngBalanceMenu.lua
+++ HadesSpeedrunningModpack/RngBalanceMenu.lua
@@ -56,86 +56,9 @@ function HSMConfigMenu.CreateRNGMenu( screen )
   })
   screen.Components["SackModdedCheckBox"].OnPressedFunctionName = "HSMConfigMenu__ToggleSackCheckBox"
 
-  -- Dropdown options
-  local maxSack2 = {}
-  local maxSack3 = {}
-  local maxSack4 = {}
-  local maxSack5 = {}
-  local minSack2 = {
-    event = function(dropdown)
-      SatyrSackControl.config.MinSack = 2
-      maxSack2.IsEnabled = true
-      maxSack3.IsEnabled = true
-      maxSack4.IsEnabled = true
-    end,
-    Text = "2",
-  }
-  local minSack3 = {
-    event = function(dropdown)
-      SatyrSackControl.config.MinSack = 3
-      maxSack2.IsEnabled = false
-      maxSack3.IsEnabled = true
-      maxSack4.IsEnabled = true
-    end,
-    Text = "3",
-  }
-  local minSack4 = {
-    event = function(dropdown)
-      SatyrSackControl.config.MinSack = 4
-      maxSack2.IsEnabled = false
-      maxSack3.IsEnabled = false
-      maxSack4.IsEnabled = true
-    end,
-    Text = "4",
-  }
-  local minSack5 = {
-    event = function(dropdown)
-      SatyrSackControl.config.MinSack = 5
-      maxSack2.IsEnabled = false
-      maxSack3.IsEnabled = false
-      maxSack4.IsEnabled = false
-    end,
-    Text = "5",
-  }
-  maxSack2 = {
-    event = function(dropdown)
-      SatyrSackControl.config.MaxSack = 2
-      minSack3.IsEnabled = false
-      minSack4.IsEnabled = false
-      minSack5.IsEnabled = false
-    end,
-    Text = "2",
-  }
-  maxSack3 = {
-    event = function(dropdown)
-      SatyrSackControl.config.MaxSack = 3
-      minSack3.IsEnabled = true
-      minSack4.IsEnabled = false
-      minSack5.IsEnabled = false
-    end,
-    Text = "3",
-  }
-  maxSack4 = {
-    event = function(dropdown)
-      SatyrSackControl.config.MaxSack = 4
-      minSack3.IsEnabled = true
-      minSack4.IsEnabled = true
-      minSack5.IsEnabled = false
-    end,
-    Text = "4",
-  }
-  maxSack5 = {
-    event = function(dropdown)
-      SatyrSackControl.config.MaxSack = 5
-      minSack3.IsEnabled = true
-      minSack4.IsEnabled = true
-      minSack5.IsEnabled = true
-    end,
-    Text = "5",
-  }
-
   -- Minimum Satyr Sack
   itemLocationY = itemLocationY + itemSpacingY
+
   ModpackMenu.Label(
       "MinimumSatyrSack",
       " - Min Tunnel for Sack: ",
@@ -144,23 +67,19 @@ function HSMConfigMenu.CreateRNGMenu( screen )
           ItemLocationY = itemLocationY,
       }
   )
-  ErumiUILib.Dropdown.CreateDropdown(
-      	screen, {
-      		Name = "SackMinDropDown",
-      		Group = "SackMinGroup",
-      		Scale = {X = .25, Y = .5},
-      		Padding = {X = 0, Y = 2},
-      		X = itemLocationX + itemSpacingX, Y = itemLocationY,
-      		GeneralFontSize = 16,
-          Font = "AlegrayaSansSCRegular",
-      		Items = {
-      			["Default"] = {Text = SatyrSackControl.config.MinSack, event = function() end},
-            minSack2,
-            minSack3,
-            minSack4,
-            minSack5,
-      		},
-      	})
+
+  ModpackMenu.DropDown(
+    "MinimumSatyrSack",
+    SatyrSackControl.config,
+    "MinSack",
+    {2, 3, 4, 5},
+    {
+      Scale = {X = .25, Y = .5},
+      Padding = {X = 0, Y = 2,
+      X = itemLocationX + itemSpacingX,
+    }
+  )
+
   itemLocationY = itemLocationY + itemSpacingY
 
   -- Max
@@ -172,24 +91,20 @@ function HSMConfigMenu.CreateRNGMenu( screen )
           ItemLocationY = itemLocationY,
       }
   )
-  ErumiUILib.Dropdown.CreateDropdown(
-        screen, {
-          Name = "SackMaxDropDown",
-          Group = "SackMaxGroup",
-          Scale = {X = .25, Y = .5},
-          Padding = {X = 0, Y = 2},
-          X = itemLocationX + itemSpacingX, Y = itemLocationY,
-          GeneralFontSize = 16,
-          Font = "AlegrayaSansSCRegular",
-          Items = {
-            ["Default"] = {Text = SatyrSackControl.config.MaxSack, event = function() end},
-            maxSack2,
-            maxSack3,
-            maxSack4,
-            maxSack5,
-          },
-        })
+
+  ModpackMenu.DropDown(
+    "MaximumSatyrSack",
+    SatyrSackControl.config,
+    "MaxSack",
+    {2, 3, 4, 5}
+    {
+      Scale = {X = .25, Y = .5},
+      Padding = {X = 0, Y = 2},
+      X = itemLocationX + itemSpacingX,
+    }
+  )
   itemLocationY = itemLocationY + itemSpacingY
+
   updateSackCheckbox(screen, screen.Components["SackModdedCheckBox"])
 
   -----------------
